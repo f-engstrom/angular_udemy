7e57d92440e4d2b16e04127f01a2f93504a398ec
Frans Engström <frans.engstrom@learnet.se>
2020-11-06 09:36:33 +0100

    follow along

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CockPitComponent } from './cock-pit/cock-pit.component';
+import { ServerElementComponent } from './server-element/server-element.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CockPitComponent,
+    ServerElementComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
